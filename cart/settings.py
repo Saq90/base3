360c014edc7fa5e5073da52692a45936f9cc9aee
Saquib <saquibjawed0@gmail.com>
2021-01-26 01:47:31 +0530

    initial commit

--- cart/settings.py
+++ cart/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
-
 django_heroku.settings(locals())
